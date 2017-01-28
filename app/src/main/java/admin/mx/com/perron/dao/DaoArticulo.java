6292949891597244eee57b976b33d389dfcd6488
george-lap\george <alberto.araiza88@gmail.com>
2017-01-28 12:24:50 -0600

    cambios realizados por Alberto

--- app/src/main/java/admin/mx/com/perron/dao/DaoArticulo.java
+++ app/src/main/java/admin/mx/com/perron/dao/DaoArticulo.java
@@ -1,13 +1,8 @@
 package admin.mx.com.perron.dao;
 import android.content.Context;
-import android.os.AsyncTask;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import admin.mx.com.perron.activities.AgregarArticuloActivity;
 import admin.mx.com.perron.entities.Articulo;
-import admin.mx.com.perron.utils.MyProperties;
 
 public class DaoArticulo  {
 
@@ -15,22 +10,24 @@ public class DaoArticulo  {
     }
     private Context ctx;
     private int op;
+    private int position;
     private AgregarArticuloActivity agregarArticuloActivity;
-    public DaoArticulo(Context ctx, int op) {
+    public DaoArticulo(Context ctx, int op, int position) {
+        this.position = position;
         this.ctx = ctx;
         this.op = op;
     }
 
     public void getListaArticulo(Articulo art){
-        DatabaseObject dao = new DatabaseObject(ctx,art, op);
+        DatabaseObject dao = new DatabaseObject(ctx,art, op,position);
         dao.execute();
     }
     public void saveArticulo(Articulo articulo){
-        DatabaseObject dao = new DatabaseObject(ctx, agregarArticuloActivity, articulo, op);
+        DatabaseObject dao = new DatabaseObject(ctx, agregarArticuloActivity, articulo, op, position);
         dao.execute();
     }
     public void actualizarArticulo(Articulo articulo){
-        DatabaseObject dao = new DatabaseObject(ctx, articulo, op);
+        DatabaseObject dao = new DatabaseObject(ctx, articulo, op,position);
         dao.execute();
     }
 
